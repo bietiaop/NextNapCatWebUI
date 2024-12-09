dbfb6a67c3e52fe514acb637165a204f551b4309
bietiaop <1527109126@qq.com>
2024-12-09 10:40:59 +0800

    fix(monaco): 构建后的路径

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig, normalizePath } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import path from 'node:path'
 
+const monacoEditorPath = normalizePath(
+  path.resolve(__dirname, 'node_modules/monaco-editor/min/vs')
+)
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: monacoEditorPath,
+          dest: 'monaco-editor/min'
+        }
+      ]
+    })
+  ],
   base: './',
   server: {
     // proxy: {
